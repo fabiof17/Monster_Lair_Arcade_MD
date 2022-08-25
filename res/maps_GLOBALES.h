1cde152e086268e766c99c085d067ba857909b37
fabio <fabiof17@hotmail.fr>
2022-08-25 15:08:04 +0200

    MAJ

--- res/maps_GLOBALES.h
+++ res/maps_GLOBALES.h
@@ -5,7 +5,6 @@ extern const TileSet tileset_NIVEAU1_WINDOW;
 extern const TileSet tileset_TETE_H;
 extern const TileSet tileset_TETE_F;
 extern const TileSet tileset_TETE_MORT;
-extern const TileSet tileset_CONTINUE;
 extern const TileSet tileset_BARRE_ORANGE;
 extern const TileSet tileset_BARRE_JAUNE;
 extern const TileSet tileset_BARRE_VERTE2;
@@ -15,7 +14,6 @@ extern const Image image_NIVEAU1_WINDOW;
 extern const Image image_TETE_H;
 extern const Image image_TETE_F;
 extern const Image image_TETE_MORT;
-extern const Image image_CONTINUE;
 extern const Image image_BARRE_VERTE1;
 extern const Image image_BARRE_VIERGE;
 
